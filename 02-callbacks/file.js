6bef214c7f27827d6d575ff9751deff686479770
Sema García <jose-manuel.garcia@gft.com>
2017-11-21 18:58:32 +0100

    Complete example implementation

--- 02-callbacks/file.js
+++ 02-callbacks/file.js
@@ -1,8 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile('./file.txt', 'utf8', (err, data) => {
-    if(err || !data) return null;
-    let lines = data.split('\n');
-    console.log('Lines: ', lines);
-});
+const readStream = getObjectToReadFileStream();
+readStream.on('chunk', (chunk) => console.log('New chunk read: ', chunk));
+readStream.on('error', (err) => console.log('Error!!!: ', err));
+readStream.on('end', () => console.log('End!'));
+
+readStream.read('./file.txt');          // Valid file
+readStream.read('./fileNotFound.txt');  // Invalid file
+
+
+function getObjectToReadFileStream() {
+    let chunkCb = null,         // Callback for each chunk read
+        errorCb = null,         // Callback for error found
+        completeCb = null;      // Callback for entire file read
+
+    return {
+        on: (event, callback) => {
+            switch(event) {
+                case 'chunk': chunkCb = callback; break;
+                case 'error': errorCb = callback; break;
+                case 'end': completeCb = callback; break;
+            }
+        },
+        read: (pathFile) => {
+            fs.readFile(pathFile, 'utf8', (err, data) => {
+                if(err || !data) {
+                    errorCb(err);
+                    return;
+                }
+                data.split('\n').forEach(chunkCb);
+                completeCb();
+            });
+        }
+    }
+}
