d75976b710f26c48869fe23bfa14885d4e9df295
Sema García <jose-manuel.garcia@gft.com>
2017-11-25 08:41:00 +0100

    Creating an observable and adding an operator

---
+++ 04-seems-an-observable/createObs2.js
@@ -0,0 +1,41 @@
+// Creating an observable with a subscribe property
+function createObservable(subscribe) {
+    return {
+        subscribe: subscribe,
+        filter: filter
+    };
+}
+
+function filter(myFilterFunction) {
+    // Address to lionObservable (owner object). So, the input of filter operation is an observable
+    const inputObservable = this;
+    const outputObservable = createObservable((outputObserver) => {
+        inputObservable.subscribe({
+            next: (d) => {
+                const output = myFilterFunction(d);
+                if(output)
+                    outputObserver.next(d);
+            },
+            error: (e) => outputObserver.error(e),
+            complete: () => outputObserver.complete()
+        });
+    });
+    return outputObservable;
+}
+
+// Our observer 
+const observer = {
+    next: (data) => console.log(`Ohhh! Lion has said: ${data}`),
+    error: (error) => console.log('Error! ', error),
+    complete: () => console.log('End!')
+};
+
+const lionObservable = createObservable((obs) => {
+    console.log('The lion is there... listen it!');
+    ["GRR", "GRRRRRR", "GRRR", "UOOAHHHH"].forEach(obs.next);
+    obs.complete();
+});
+
+lionObservable
+    .filter(sound => sound.length > 4)
+    //.subscribe(observer);
